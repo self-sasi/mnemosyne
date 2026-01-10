759100285481097e9670fa00049bda4a1c1e85bc
self-sasi <sarthaks281bond@gmail.com>
2026-01-10 01:04:31 -0700

    expand Request structs to include db connection credentials and target/source paths

--- pkg/api/request.go
+++ pkg/api/request.go
@@ -1,5 +1,25 @@
 package api
 
-type BackupRequest struct{}
+// DBCredentials defines the fields required to establish
+// a connection to a database.
+type DBCredentials struct {
+	Host     string
+	Port     int
+	DBName   string
+	UserName string
+	Password string
+}
 
-type RestoreRequest struct{}
+// BackupRequest defines the fields required to perform
+// a database backup operation
+type BackupRequest struct {
+	DBCredentials DBCredentials // info to connect with db
+	TargetPath    string        // where to store the backup files
+}
+
+// RestoreRequest defines the fields required to perform
+// a database restoration operation
+type RestoreRequest struct {
+	DBCredentials DBCredentials // info to connect with db
+	SourcePath    string        // where to retrieve backup files from
+}
