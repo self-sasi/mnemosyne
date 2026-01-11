b9b9d81271195ebcdda46eec7dd64b1dac26f99e
self-sasi <sarthaks281bond@gmail.com>
2026-01-10 20:41:41 -0700

    implement mongodb engine using engine spi provided by pkg/api

---
+++ pkg/engines/mongodb/mongo_engine.go
@@ -0,0 +1,28 @@
+package mongodb
+
+import (
+	"context"
+
+	"github.com/self-sasi/mnemosyne/pkg/api"
+)
+
+const mongoEngineName api.EngineName = "mongodb"
+
+type MongoEngine struct {
+}
+
+func NewEngine() *MongoEngine {
+	return &MongoEngine{}
+}
+
+func (mongoEngine *MongoEngine) Name() api.EngineName {
+	return mongoEngineName
+}
+
+func (mongoEngine *MongoEngine) Backup(ctx context.Context, request api.BackupRequest) (api.BackupResponse, error) {
+	return api.BackupResponse{}, nil
+}
+
+func (mongoEngine *MongoEngine) Restore(ctx context.Context, request api.RestoreRequest) (api.RestoreResponse, error) {
+	return api.RestoreResponse{}, nil
+}
